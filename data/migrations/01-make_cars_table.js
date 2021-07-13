0338d14dc99bb805a8d6e8c68ef4fc2d21b85f18
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-13 15:15:16 -0500

    Migration file complete

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,22 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema
+    .createTable('cars', table => {
+      table.increments('car_id')
+      table.text('vin', 50)
+        .unique()
+        .notNullable()
+      table.text('make', 50)
+        .notNullable()
+      table.text('model', 50)
+        .notNullable()
+      table.integer('mileage', 50)
+        .notNullable()
+      table.text('title', 50)
+      table.text('transmission', 50)
+    })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema
+    .dropTableIfExists('cars')
 };
